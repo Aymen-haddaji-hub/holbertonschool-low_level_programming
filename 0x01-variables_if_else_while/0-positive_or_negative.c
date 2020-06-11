8b41664c2f3f652db3d9b6823e067a53d71d527a
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-06-11 14:56:24 +0100

    Random Positive or negative number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,16 +17,18 @@ int main(void)
 	/* my code */
 	if (n > 0)
 	{
-		printf("%d, is positive\n");
+		printf("%d", n);
+		printf("is positive\n");
 	}
 	else if (n < 0)
 	{
-		printf("%d, is negative\n");
+		printf("%d", n);
+		printf("is negative\n");
 	}
 	else
-		(n == 0)
 	{
-		printf("%d, is zero\n");
+		printf("%d", n);
+		printf("is zero\n");
 	}
 	return (0);
 }
