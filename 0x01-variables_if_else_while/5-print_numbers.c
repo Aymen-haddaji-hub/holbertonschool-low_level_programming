e8a56d91ceda9095e8d54de8723c35a75f47add0
Aymen-haddaji-hub <62173576+Aymen-haddaji-hub@users.noreply.github.com>
2020-06-11 23:01:57 +0100

    Nuuumbers here

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main-prints all numbers of base 10 starting from 0
+ *Return: 0;
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
