3522b7c63282ddc0aac12fa2b980458bb8c0a743
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-06-16 21:28:37 +0100

    10Times_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+{
+	char b;
+	int i;
+
+	while (i++ <= 9)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+			_putchar(b);
+			_putchar("\n");
+	return (0);
+	}
+}
