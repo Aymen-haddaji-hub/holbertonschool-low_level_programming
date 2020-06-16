e2070457b35cebe48a34e911d8cb9e0abcab52c4
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-06-16 08:57:35 +0100

    First function

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *
+ *main boid
+ *
+ *return 0
+ **/
+
+int main(void)
+{
+	char holberton;
+		_putchar('h');
+		_putchar('o');
+		_putchar('l');
+		_putchar('b');
+		_putchar('e');
+		_putchar('r');
+		_putchar('t');
+		_putchar('o');
+		_putchar('n');
+		_putchar('\n');
+		return (0);
+}
