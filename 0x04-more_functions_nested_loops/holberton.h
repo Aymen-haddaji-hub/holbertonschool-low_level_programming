decfb78458c010505ccc00d2a509e78acea23d9e
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-06-18 21:52:46 +0100

    update header

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -7,4 +7,8 @@ int mul(int a, int b);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
 #endif
