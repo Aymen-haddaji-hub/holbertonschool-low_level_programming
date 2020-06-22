b8640b911d60e1161d6a8465cf1015f8ac356b52
Aymen-haddaji-hub <62173576+Aymen-haddaji-hub@users.noreply.github.com>
2020-06-22 22:24:24 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line.
- *
- * Return: Always 1.
- */
+ *main - prints line
+ *Return: 1
+*/
+
 int main(void)
 {
-	fwrite(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
+	int l;
 
+	l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 	return (1);
 }
