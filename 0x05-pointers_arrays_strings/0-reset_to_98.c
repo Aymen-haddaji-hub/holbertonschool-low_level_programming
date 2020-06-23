e861b4d090368cb77892d111ad2640a81343973c
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-06-23 09:25:23 +0100

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- *reset_to_98 - takes a pointer and updates the value to 98
- *@*n: variable adress value (pointer)
- * Return: Always 0;
- */
+*reset_to_98 - takes a pointer and updates the value to 98
+*@n:var.
+*Return: Always 0;
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
