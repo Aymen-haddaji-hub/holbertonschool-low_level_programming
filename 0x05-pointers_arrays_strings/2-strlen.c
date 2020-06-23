58744130dcaceb7a9211d85b36cbcfd771506c4c
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-06-23 09:31:50 +0100

    String_length_function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: character of string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
