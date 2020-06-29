229aa28f5b272ecb58bf4efe1c542abcf5fd8704
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-06-29 13:07:00 +0100

    strchr func

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
  * _strchr - check the code for Holberton School students.
- *@s string 
- *@c char we search
+ *@s: string
+ *@c: char we search
  * Return: c if char available null if does not exist.
  */
 char *_strchr(char *s, char c)
