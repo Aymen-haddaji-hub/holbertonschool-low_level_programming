a9503993ff10bfb20257c344c96234de07355545
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-07-01 22:25:27 +0100

    fixing code

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
-
 /**
- *_print_rev_recursion function that prints a string in reverse.
- *
- *@s: a pointer with type char.
- **/
-
+* _print_rev_recursion - a function that prints a string in reverse.
+* @s: A pointer to string
+* Return: the char
+*/
 void _print_rev_recursion(char *s)
 {
+
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(s[0]);
+		_putchar (s[0]);
 	}
 }
