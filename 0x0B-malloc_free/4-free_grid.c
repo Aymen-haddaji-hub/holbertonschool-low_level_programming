c62c8fd2ab49d974932a5b24b895479deba19ef5
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-07-09 12:36:02 +0100

    Free_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * free_grid - function to free memory from grid
+ * free_grid - frees 2d grid priviously created
  * @grid: grid to free;
  * @height: height of grid;
  * Return: none
