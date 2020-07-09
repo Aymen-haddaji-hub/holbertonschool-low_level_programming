60e47f8c796fb9acdb9e4025b5194d5c0505ac7e
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-07-09 16:16:36 +0100

    Header file

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,12 @@
+int _strlen(char *s);
+void error_msg(void);
+int _isNum(char *num);
+void rev_string(char *s);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
+void *_memset(void *s, int b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
