8b2dbda6745a2722ed23b04dbe1c028093e85c55
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-07-15 19:16:14 +0100

    betty style fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints the name given.
  * @name: input name.
  * @f: function that print input;
- * Return: no return 
+ * Return: no return.
  */
 
 void print_name(char *name, void (*f)(char *))
