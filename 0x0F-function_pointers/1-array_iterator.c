a2debff3b44636a1f2d66096a27668d09f923c26
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-07-15 19:04:25 +0100

    arry func

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - Execute function on each element of array
+ * @array: int array
+ * @size: size of array
+ * @action: function to use on array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
