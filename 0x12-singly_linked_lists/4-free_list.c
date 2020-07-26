f5098577f15479e8b7a4b5468af90f2af3411ddf
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-07-26 20:11:10 +0100

    free list function

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free linkedlist.
+ * @head: A pointer to the first element of linkedlist.
+ * return: no return (void).
+ */
+void free_list(list_t *head)
+{
+	list_t *p;
+
+	while (head)
+	{
+		p = head->next;
+		free(head->str);
+		free(head);
+		head = p;
+	}
+}
