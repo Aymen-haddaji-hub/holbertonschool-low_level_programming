99d49304fd175c81539b3c5f73a02ac9c6544800
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-08-02 20:43:33 +0100

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all the elements of list
+ * @h: pointer to the list
+ * Return: the number of the elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
